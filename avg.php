dde1b290cebb511b6010e2701e2e816bdbdcf6a8
wrx98342 <wrx98342@student.wroclaw.merito.pl>
2026-03-10 18:31:01 +0100

    Srednia wazona

--- avg.php
+++ avg.php
@@ -22,3 +22,13 @@ $w = $w + $a;
 //wypisz wynik
 printf("%lf", $w/$n);
 ?>
+
+
+
+function familyName($fname, $year) {
+  echo "$fname Refsnes. Born in $year.<br>";
+}
+
+familyName("Hege", "1975");
+familyName("Stale", "1978");
+familyName("Kai Jim", "1983");

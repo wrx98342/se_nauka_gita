799d9b1a39f3294bf283f389e27edec9ff5e440e
wrx98342 <wrx98342@student.wroclaw.merito.pl>
2026-03-10 18:25:01 +0100

    Funkcja srednia

---
+++ avg.php
@@ -0,0 +1,24 @@
+<?php
+ 
+//Obliczanie średniej arytmetycznej
+//www.algorytm.org
+ 
+$inputy = fopen('php://stdin', "r");
+ 
+//pobierz liczbe elementow
+fscanf($inputy, "%d",$n);
+ 
+//zainicjuj sume elementow
+$w = 0.0;
+ 
+//dla kazdego elementu
+for( $i = 0; $i < $n; $i++ )
+{
+//pobierz wartosc elementu i dodaj ja do sumy
+fscanf($inputy, "%d",$a);
+$w = $w + $a;
+}
+ 
+//wypisz wynik
+printf("%lf", $w/$n);
+?>

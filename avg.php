ae96fb256305460f480c3d419425c08207027ee0
wrx98342 <wrx98342@student.wroclaw.merito.pl>
2026-03-10 18:41:00 +0100

    do zrobienia zeby byc na bierzaco

--- avg.php
+++ avg.php
@@ -1,34 +1,10 @@
 <?php
- 
-//Obliczanie średniej arytmetycznej
-//www.algorytm.org
- 
-$inputy = fopen('php://stdin', "r");
- 
-//pobierz liczbe elementow
-fscanf($inputy, "%d",$n);
- 
-//zainicjuj sume elementow
-$w = 0.0;
- 
-//dla kazdego elementu
-for( $i = 0; $i < $n; $i++ )
+funkction srednia(a, b):
 {
-//pobierz wartosc elementu i dodaj ja do sumy
-fscanf($inputy, "%d",$a);
-$w = $w + $a;
+  return (a+b)/2;
 }
- 
-//wypisz wynik
-printf("%lf", $w/$n);
+funkction srednia_wazona(a, b):
+{
+  return 0.2*a + 0.8*b;
+}
 ?>
-
-
-
-function familyName($fname, $year) {
-  echo "$fname Refsnes. Born in $year.<br>";
-}
-
-familyName("Hege", "1975");
-familyName("Stale", "1978");
-familyName("Kai Jim", "1983");
